9a4e1c42b87388a6300a5adad93f98598d64f709
harshad-dhokane <worrk.harsshad@gmail.com>
2025-05-10 23:03:54 +0530

    next commit

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code and save to test HMR.......
+          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
@@ -30,6 +30,6 @@ function App() {
       </p>
     </>
   )
-
+}
 
 export default App
